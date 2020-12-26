78ac192d0bc8408125b43d91e0ed7aa7282be7b4
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-26 14:32:13 -0500

    change the way files are found and allow the user to specify the host using cmd

--- ftp/server.py
+++ ftp/server.py
@@ -1,12 +1,14 @@
 import flask
 import os
+import sys
 
 app = flask.Flask(__name__)
 
 @app.route("/<filename>")
 def index(filename):
-    with open(os.path.join("ftp", "assets", filename), 'r') as f:
+    with open(os.path.join("assets", filename), 'r') as f:
         return f.read()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    host = sys.argv[1]
+    app.run(host=str(host))
